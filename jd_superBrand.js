ab99c5ddf90b39cac9f5c4bbc1d4755d9c3984a6
hajiuhajiu <2705322732@qq.com>
2021-12-15 17:16:47 +0800

    Add files via upload

--- jd_superBrand.js
+++ jd_superBrand.js
@@ -1,7 +1,7 @@
 /**
  特务Z
  脚本没有自动开卡，会尝试领取开卡奖励
- cron 23 8,9 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_productZ4Brand.js
+ cron 13 15,19 * * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_superBrand.js
  一天要跑2次
  */
 const $ = new Env('特务Z');
@@ -50,7 +50,7 @@ if ($.isNode()) {
         }catch (e) {
             console.log(JSON.stringify(e));
         }
-        await $.wait(1000);
+        await $.wait(11000);
     }
     if($.allInvite.length > 0 ){
         console.log(`\n开始脚本内互助\n`);
@@ -145,7 +145,7 @@ async function doTask(){
             if (signList.length === 0) {
                 console.log(`任务：${$.oneTask.assignmentName},信息异常`);
             }
-            if ($.oneTask.assignmentName.indexOf('首页下拉') !== -1) {
+            if ($.oneTask.assignmentName.indexOf('首页下拉') !== -1 || $.oneTask.assignmentDesc.includes('首页下拉')) {
                 for (let j = 0; j < signList.length; j++) {
                     if (signList[j].status === 1) {
                         console.log(`任务：${$.oneTask.assignmentName},去执行,请稍稍`);

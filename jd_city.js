dae10b8197520a3261b5791161a743535836c2c0
hajiuhajiu <2705322732@qq.com>
2022-01-10 15:12:14 +0800

    Update jd_city.js

--- jd_city.js
+++ jd_city.js
@@ -57,10 +57,10 @@ let pool = []
   } else {
     console.log(`脚本不会自动抽奖，建议活动快结束开启，默认关闭`)
   }
-  // if (process.env.CT_R != 'false') {
-  //   cookiesArr = cookiesArr.sort(() => 0.5 - Math.random())
-  //   console.log('CK顺序打乱!用来随机内部互助!,如需关闭CT_R为false')
-  // }
+  if (process.env.CT_R != 'false') {
+    cookiesArr = cookiesArr.sort(() => 0.5 - Math.random())
+    console.log('CK顺序打乱!用来随机内部互助!,如需关闭CT_R为false')
+  }
   console.log('明日可能默认开启CK打乱（随机内部互助）,如需关闭请提前设置CT_R为false')
   let res = await getAuthorShareCode('https://xr2021.coding.net/p/import-kasd/d/JDbot/git/raw/master/shareCodes/city.json')
   if (!res) {
